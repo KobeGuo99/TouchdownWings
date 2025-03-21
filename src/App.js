2c26420c0ac142d9206fc0005016e3ad95335404
Kobe Guo <kobeguo02@gmail.com>
2025-03-21 15:12:38 -0500

    removed closing notice

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div>
       <Header />
-      <AlertMessage message={alertMessage} />
+      {/* <AlertMessage message={alertMessage} /> */}
       <HoursContact />
       <Menu />
       {/* <FoodGallery /> */}
