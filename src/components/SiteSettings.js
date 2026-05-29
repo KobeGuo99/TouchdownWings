40574ebb380b1491aa4dd1ac14e70618332187b9
Chris Guo <chris@Chriss-MacBook-Pro-7.local>
2026-05-28 22:01:57 -0500

    updating closing messsage

--- src/components/SiteSettings.js
+++ src/components/SiteSettings.js
@@ -2,5 +2,5 @@ export const specialClosure = {
   // Set this to true when the restaurant is closed outside normal hours.
   isActive: true,
   message:
-    "Closed on Fri., March 29 - Tues. June 9. Will reopen on Wed., June 10. Sorry for the inconvenience!",
+    "Closed on Fri., May 29 - Tues. June 9. Will reopen on Wed., June 10. Sorry for the inconvenience!!",
 };
