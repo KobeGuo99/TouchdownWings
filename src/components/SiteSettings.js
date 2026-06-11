32e39a1b375dec6f271b1c623714eee7aa2f75f6
Chris Guo <chris@Chriss-MacBook-Pro-7.local>
2026-06-11 11:23:41 -0500

    6/11/26 ending special closure

--- src/components/SiteSettings.js
+++ src/components/SiteSettings.js
@@ -1,6 +1,6 @@
 export const specialClosure = {
   // Set this to true when the restaurant is closed outside normal hours.
-  isActive: true,
+  isActive: false,
   message:
     "Closed on Fri., May 29 - Wed., June 10. Will reopen on Thurs., June 11. Sorry for the inconvenience!!",
 };
